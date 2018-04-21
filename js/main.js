e205936b8e4ccb60e22f7e504af13b42981581ca
Beth Williams <bwilliams1014@gmail.com>
2018-04-21 18:23:45 -0400

    Fix interactivity in events section so it displays properly

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 $('#services-images').backstretch(['img/design.jpg', 'img/pruning.jpg', 'img/gravel-walk.jpg', 'img/edging.jpg', 'img/sod-rolling.jpg', 'img/planting-tree.jpg', 'img/rake-mulch.jpg', 'img/laying-pavers.jpg'], {duration: 3000, fade: 750});
 
-$('.smoothscroll').smoothScroll({'offset':'170'});
+$('.smoothscroll').smoothScroll({'offset':'20'});
 
 $('.services .bold').hover(function() {
 	$(this).css('cursor', 'pointer');
@@ -12,13 +12,14 @@ $('.services .bold').on('click', function() {
 	$(this).siblings('.hidden-info').slideToggle('slow');
 	});
 
+
+
 var date = new Date();
 var currentTime = date.getHours();
 var open = null;
 if (currentTime > 7 < 19)
 	open = 'OPEN';
 else open = 'CLOSED';
-
 document.getElementById('time').innerHTML+= 'Hours: 8:00 am - 7:00 pm. We are now ' + open + '.';
 
 });
