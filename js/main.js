fccdf6b41b8dc4e045a42eb03a33941ac152ad9e
Beth Williams <bwilliams1014@gmail.com>
2018-04-21 19:26:52 -0400

    Add hover animation to figures

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
 $(document).ready(function() {
 
-$('#services-images').backstretch(['img/design.jpg', 'img/pruning.jpg', 'img/gravel-walk.jpg', 'img/edging.jpg', 'img/sod-rolling.jpg', 'img/planting-tree.jpg', 'img/rake-mulch.jpg', 'img/laying-pavers.jpg'], {duration: 3000, fade: 750});
-
+// add smoothscroll plugin
 $('.smoothscroll').smoothScroll({'offset':'20'});
 
+// add images to services element with backstretch plugin
+$('#services-images').backstretch(['img/design.jpg', 'img/pruning.jpg', 'img/gravel-walk.jpg', 'img/edging.jpg', 'img/sod-rolling.jpg', 'img/planting-tree.jpg', 'img/rake-mulch.jpg', 'img/laying-pavers.jpg'], {duration: 3000, fade: 750});
+
+// change cursor when hovering over text in services section
 $('.services .bold').hover(function() {
 	$(this).css('cursor', 'pointer');
-});
+	});
 
+// show hidden information when text is clicked on
 $('.services .bold').on('click', function() {
 	$(this).siblings('.hidden-info').slideToggle('slow');
 	});
 
-
-
+// determine if store is open or closed; add content to HTML
 var date = new Date();
 var currentTime = date.getHours();
 var open = null;
-if (currentTime > 7 < 19)
+if (currentTime >= 7 && currentTime < 19)
 	open = 'OPEN';
-else open = 'CLOSED';
-document.getElementById('time').innerHTML+= 'Hours: 8:00 am - 7:00 pm. We are now ' + open + '.';
+	else open = 'CLOSED';
+document.getElementById('time').innerHTML+= 'Hours: 8:00 am - 7:00 pm.  We are now ' + open + '.';
 
 });
