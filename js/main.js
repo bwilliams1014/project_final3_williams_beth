099811832999e6c6717e60acd350a78ec36d6074
Beth Williams <bwilliams1014@gmail.com>
2018-04-21 10:11:32 -0400

    Add slide toggle on click to services section

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@ $(document).ready(function() {
 $('#services-images').backstretch(['img/design.jpg', 'img/pruning.jpg', 'img/gravel-walk.jpg', 'img/edging.jpg', 'img/sod-rolling.jpg', 'img/planting-tree.jpg', 'img/rake-mulch.jpg', 'img/laying-pavers.jpg'], {duration: 3000, fade: 750});
 
 $(function(){
-		$('#menu').slicknav();
+	$('#menu').slicknav();
 	});
 
+$('.services .bold').on('click', function() {
+	$(this).siblings('.hidden-info').slideToggle('slow');
+	});
 
 });
